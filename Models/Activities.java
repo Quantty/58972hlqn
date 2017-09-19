6bc243c5fccc370ddc7e7946cb74175a645e3f37
Oliver Christensen <christensenoliver27@gmail.com>
2017-09-19 09:20:59 +0200

    added models package with Activities class

---
+++ Models/Activities.java
@@ -0,0 +1,22 @@
+package sample.Models;
+
+/**
+ * Created by narto_000 on 9/19/2017.
+ */
+public class Activities {
+
+    private String name;
+    private int ageLimit;
+    private double heightLimit;
+    private String description;
+
+    public Activities(String name, int ageLimit, double heightLimit, String description) {
+
+        name = name;
+        ageLimit = ageLimit;
+        heightLimit = heightLimit;
+        description = description;
+
+    }
+
+}
